bd607a9c5400dc2ac2970a016e38cd51960f0027
takuya nosaka <takuya@miyamuramimi-no-MacBook-Air.local>
2021-01-01 23:54:50 +0900

    issue5の実装 #5

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from blog.models import Category, Tag, Post
+from blog.models import Category, Tag, Post, ContentImage
+
+
+class ContentImageInLine(admin.TabularInline):
+    model = ContentImage
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        ContentImageInLine,
+    ]
 
 
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
