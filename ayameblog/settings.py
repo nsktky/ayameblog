39846916d9b2f5b46b69bdc877de684fb4c7167c
takuya nosaka <takuya@miyamuramimi-no-MacBook-Air.local>
2021-01-06 23:51:17 +0900

    setting.pyのシークレットキー対策

--- ayameblog/settings.py
+++ ayameblog/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+# 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9)ko+s8t)y5%t@0ypwju=je3jb9v9%+$&xs3$@^ibyl&fze5ey'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
